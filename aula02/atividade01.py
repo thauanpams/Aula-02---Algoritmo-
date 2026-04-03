8217478fab9137b81c163b4513773ae5c808b1b7
thauanpams <thauanmarins@gmail.com>
2026-04-02 21:00:25 -0300

    inicializa o repositório

---
+++ aula02/atividade01.py
@@ -0,0 +1,15 @@
+# numero = float(input(f'Digite um número') )
+# numero_sucessor = numero + 1
+# numero_antecessor = numero - 1
+# print(f'Número: {numero}, número sucessor: {numero_sucessor}, , número antecessor:{numero_antecessor}')
+
+
+
+#salario de uma pessoa
+#salario acrescido de 15%
+
+salario = float(input(f'Salario de uma pessoa:'))
+salario_acrescido = salario * 1.15
+
+print(f'Salario acrescido: {salario_acrescido:.2f}')
+
---
+++ aula02/exemplo 2.py
@@ -0,0 +1,81 @@
+# nome = "Joao"
+# idade = 18
+
+# print(nome)
+# print(f'Olá,{nome} tudo bem?, você tem quantos anos? {idade} ')
+# ----------------------------------------------------------
+
+# Atividade de fixação
+
+# nome = "Aline"
+# bairro = "Santa Barbara"
+# profissao = "Engenheira"
+
+# print(f'Olá, {nome}')
+# print(f'Onde você mora? {bairro}')
+# print(f'Qual é a sua profissão? {profissao}')
+
+
+# ----------------------------------------
+# Operadores
+# preco = 20
+# quantidade = 3
+# total = preco * quantidade 
+# print (f'Qual é o total {total}')
+
+# ----------------------------------------------
+# Atividade 2
+
+
+# salario = 12000
+# bonus = 880 
+# total = salario + bonus 
+
+# print(f'Qual será o salario mais a bonificação? {total}')
+
+
+
+# ---------------------------------------
+# Exemplo de entrada de valores pelo usuário:
+
+# nome = input('Informe o nome: ')
+# print (f'Olá {nome}!')
+
+
+# ----------------------------------------------
+# Atividade de  Fixação
+
+
+
+# salario = float(input('Qual é o seu salário? '))
+# bonus = float(input('Qual é o seu bônus? '))
+# total_salario = salario + bonus
+# print(f'Qual é o salario total? {total_salario}')
+
+# string = texto
+# float = Números decimais
+# int = Números Inteiros
+# Boleano = True or False
+
+
+# Cálculo de desconto 
+
+valor = float(input(f'Valor do produto: '))
+quantidade = float(input('Quantidade: '))
+total = valor * quantidade
+desconto = total * 0.1
+valor_final = total - desconto
+
+
+print(f'Valor final{desconto}')
+print(f'O valor do desconto foi de {desconto}')
+
+
+
+
+
+
+
+
+
+
